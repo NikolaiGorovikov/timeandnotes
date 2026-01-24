11043368d63e1bf1b6acce3dc6067a1d6408fc0e
Nikolai Gorovikov <nikolaigorovikov@nikolais-mini.home>
2026-01-24 16:39:41 -0500

    Unspecified changes

--- www/src/js/index.js
+++ www/src/js/index.js
@@ -4,7 +4,7 @@ export function main() {
     /* =========================
        State & constants
        ========================= */
-    const DEFAULT_NOTE_TEXT = "$x + y = x^2$";
+    const DEFAULT_NOTE_TEXT = "tatiana hello";
     const MAX_SIZE = 25;
     const MIN_SIZE = 8;
 
